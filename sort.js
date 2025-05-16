7144ba18eaf6cff9fc1c89644cc8285fc0297297
Bastien-POMMIER <bastienp14@laposte.net>
2025-05-16 12:13:56 +0200

    Create sort.js
    Added sorting functions in a new file

---
+++ sort.js
@@ -0,0 +1,31 @@
+/**
+ * @description   Trie un tableau selon une colonne dans l'ordre crooissant
+ * @param table   Tableau à trier
+ * @param filter   Colonne selon laquelle trier le tableau
+ * @author Bastien POMMIER
+ */
+function sortTable(table, filter) {
+    table.forEach((object, id) => {   // "object" n'est pas utile, on utilise juste l'index
+        compareData(table, id, filter);
+    });
+}
+
+
+
+/**
+ * @description   Compare deux lignes successives d'un tableau, puis les trie dans l'ordre croissant
+ * @param table   Tableau contenant les données (objets)
+ * @param i   Indice de la 2e lignes
+ * @param filter   Colonne selon laquelle trier le tableau
+ * @author Bastien POMMIER
+ */
+function compareData(table, i, filter) {
+    if (i == 0)   return;
+    filter.toString();
+
+    if (table[i][filter] < table[i-1][filter]) {   // Si l'élément est plus petit que le précédent...
+        [table[i], table[i-1]]   =   [table[i-1], table[i]];   // ... on inverse les 2
+
+        compareData(table, i-1, filter);  // Et on revient en arrière dans le tableau jusqu'à avoir remis l'élément à sa place
+    }
+}

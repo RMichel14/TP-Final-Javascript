d8aadaec9a6e1f692947c084760144f434535c17
Raphaël Michel <raphaelmichel@outlook.fr>
2025-05-23 01:21:58 +0200

    Update gestion_de_donnees.js
    Ajouts des appels aux fonctions pour insérer les filtres dans le HTML de manière dynamique.
    Fonctions concernées :
    - remplirFiltresDynamique();
    - mettreFiltresEnMajuscule();
    - supprimerDoublonsFiltres()

--- gestion_de_donnees.js
+++ gestion_de_donnees.js
@@ -2,6 +2,9 @@ function main() {
     const donneesMusees = transformationDatabase(data);
     let donneesMuseesTrie = donneesMusees;
     sortTable(donneesMuseesTrie, "annee");
+    remplirFiltresDynamique(donneesMuseesTrie);
+    mettreFiltresEnMajuscule();
+    supprimerDoublonsFiltres();
     renderTable(donneesMuseesTrie);
 }
 

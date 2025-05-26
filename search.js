108d0e8dec796f5c6f0dfc1c52631044b8e029a7
Raphaël Michel <raphaelmichel@outlook.fr>
2025-05-26 20:18:23 +0200

    Create search.js
    Modifications par @Nawalbis

---
+++ search.js
@@ -0,0 +1,16 @@
+function filterByQuery(data, query) {
+    const lowerQuery = query.toLowerCase(); //convertit le texte en minuscules
+
+    return data.filter(item => {//on verifie que nom est bien un char puis on le convertit en minuscules
+        const nom = item.nom;
+        return typeof nom === "string" && nom.toLowerCase().includes(lowerQuery);
+    });
+}
+
+function search(donneesMusees) {
+    searchInput.addEventListener("input", () => { //a chaque saisie on recupere ce qui est tapé, on filtre le tableau et on affiche
+        const query = searchInput.value;//on recupere ce qu'a ecrit l'user dans la barre de recherche 
+        const filteredData = filterByQuery(donneesMusees, query); // on prend en paramètres le tableau contenant les donnees du musée et ce qu'à écrit l'user pour retourner le tableau modifié
+        renderTable(filteredData);
+    });
+}
